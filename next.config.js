181e7ccc47ec44c7249e274ff4fc63dc320bc65f
fiqgant <fiqgant@gmail.com>
2023-08-28 14:34:02 +0700

    fix some

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+};
 module.exports = nextConfig;
